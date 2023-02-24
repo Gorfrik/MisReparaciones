7ae8efd527e2232b0d412b4865c6bc51aede04be
Oscar <Retorne_56@hotmail.com>
2023-02-24 13:46:41 +0100

    asdg

--- src/misreparaciones/MisReparaciones.java
+++ src/misreparaciones/MisReparaciones.java
@@ -17,11 +17,7 @@ public class MisReparaciones {
         
         
         System.out.println();
-<<<<<<< HEAD
         System.out.println("+------------------=MENU=---------------+");
-=======
-        System.out.println("+-----------------=MENU=--------+");
->>>>>>> 99e7fc6 (asd)
         System.out.println("|----------------------------------------|");
         System.out.println("| 1).Dar de alta un coche                |");
         System.out.println("| 2).Ver datos de todos los coches       |");
