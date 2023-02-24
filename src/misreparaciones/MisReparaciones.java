f7d4eb08004ee72f870498ed97a9ccee516f807a
Oscar <Oscar@example.com>
2023-02-24 16:37:00 +0100

    rtest

--- src/misreparaciones/MisReparaciones.java
+++ src/misreparaciones/MisReparaciones.java
@@ -19,7 +19,7 @@ public class MisReparaciones {
         System.out.println();
 
         
-        
+        System.out.println("");
         System.out.println("+------------------=MENU=---------------+");
         System.out.println("|---------------------------|");
         System.out.println("| 1).Dar de alta un coche                |");
